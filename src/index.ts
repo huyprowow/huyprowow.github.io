a5244e9767d74b3c3ff81cc245bd05247c200686
Huypro <huyprowow@gmail.com>
2021-11-30 17:09:20 +0700

    them ti d3 vua hoc :)))))

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 //Xóa thư mục node_modules khỏi kho lưu trữ git
 //git rm -r --cached node_modules
 
+/*---------- Nav menu ---------*/
+
 const menu = document.querySelector("#menu") as HTMLElement;
 const navBar = document.querySelector("#navbar") as HTMLElement;
 menu.addEventListener("click", () => {
   menu.style.visibility = "hidden";
-  navBar.style.right='1em';
+  navBar.style.right = "1em";
 });
 navBar.addEventListener("click", () => {
   menu.style.visibility = "visible";
-  navBar.style.right='-255px';
+  navBar.style.right = "-255px";
 });
 // console.log(menu, navBar);
+
+/*--------- Scroll top btn ---------*/
 
 const scrollBtn = document.querySelector("#scroll-top") as HTMLElement;
 scrollBtn.addEventListener("click", () => {
@@ -26,7 +30,7 @@ const showScrollButton = () => {
   // console.log(demoCalc);
   // console.log(pixelsY);
   let y = window.scrollY;
-  
+
   if (y > 500) {
     scrollBtn.style.visibility = "visible";
   } else {
@@ -34,3 +38,53 @@ const showScrollButton = () => {
   }
 };
 window.addEventListener("scroll", showScrollButton);
+
+/*----------------- D3 -------------*/
+
+//npm i -s @types/d3
+
+const datas = [
+  [
+    ["Javascript", 70],
+    ["NodeJs", 40],
+    ["TypeScript", 40],
+  ],
+  [
+    ["Mysql", 30],
+    ["MongoDB", 50],
+  ],
+  [
+    ["React", 70],
+    ["Redux", 30],
+    ["Scss",40],
+    ["Express", 40],
+    ["BootStrap", 20],
+    ["JQuery", 20],
+  ],
+];
+
+type combine = string | number;
+
+const sections = ["#d3-language", "#d3-database", "#d3-framework"];
+sections.forEach((section, i) => {
+  let tr1 = d3.select(`${section}`).append("tr1");
+  tr1
+    .append("td")
+    .selectAll("p")
+    .data(datas[i])
+    .enter()
+    .append("p")
+    .text((d: combine[]) => d[0])
+    .attr("class", "d3-label");
+
+  tr1
+    .append("td")
+    .style("width", "100%")
+    .selectAll("p")
+    .data(datas[i])
+    .enter()
+    .append("p")
+    .text((d: combine[]) => d[1] + "%")
+    .attr("class", "d3-bar")
+    .style("width", (d: combine[]) => d[1] + "%");
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,19 @@
 "use strict";
 //Xóa thư mục node_modules khỏi kho lưu trữ git
 //git rm -r --cached node_modules
+/*---------- Nav menu ---------*/
 const menu = document.querySelector("#menu");
 const navBar = document.querySelector("#navbar");
 menu.addEventListener("click", () => {
     menu.style.visibility = "hidden";
-    navBar.style.right = '1em';
+    navBar.style.right = "1em";
 });
 navBar.addEventListener("click", () => {
     menu.style.visibility = "visible";
-    navBar.style.right = '-255px';
+    navBar.style.right = "-255px";
 });
 // console.log(menu, navBar);
+/*--------- Scroll top btn ---------*/
 const scrollBtn = document.querySelector("#scroll-top");
 scrollBtn.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -32,3 +34,46 @@ const showScrollButton = () => {
     }
 };
 window.addEventListener("scroll", showScrollButton);
+/*----------------- D3 -------------*/
+//npm i -s @types/d3
+const datas = [
+    [
+        ["Javascript", 70],
+        ["NodeJs", 40],
+        ["TypeScript", 40],
+    ],
+    [
+        ["Mysql", 30],
+        ["MongoDB", 50],
+    ],
+    [
+        ["React", 70],
+        ["Redux", 30],
+        ["Scss", 40],
+        ["Express", 40],
+        ["BootStrap", 20],
+        ["JQuery", 20],
+    ],
+];
+const sections = ["#d3-language", "#d3-database", "#d3-framework"];
+sections.forEach((section, i) => {
+    let tr1 = d3.select(`${section}`).append("tr1");
+    tr1
+        .append("td")
+        .selectAll("p")
+        .data(datas[i])
+        .enter()
+        .append("p")
+        .text((d) => d[0])
+        .attr("class", "d3-label");
+    tr1
+        .append("td")
+        .style("width", "100%")
+        .selectAll("p")
+        .data(datas[i])
+        .enter()
+        .append("p")
+        .text((d) => d[1] + "%")
+        .attr("class", "d3-bar")
+        .style("width", (d) => d[1] + "%");
+});
